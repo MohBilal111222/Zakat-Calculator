60f479a558fdf2f30aa6991ec9410d7723c4d0f2
Bilal <mohammadbilal91112@gmail.com>
2024-05-24 03:20:31 +0500

    Update app.js

--- app.js
+++ app.js
@@ -3,16 +3,16 @@
 
 
 function calculateZakat() {
-    // Get input values
+    // Get input values in javascript
     var goldValue = parseFloat(document.getElementById("goldValue").value);
     var silverValue = parseFloat(document.getElementById("silverValue").value);
     var cashValue = parseFloat(document.getElementById("cashValue").value);
     var otherAssetsValue = parseFloat(document.getElementById("otherAssetsValue").value);
     
-    // Zakat calculation (2.5% of total)
+    // Zakat calculation (2.5% of total);
     var totalWealth = goldValue + (silverValue * 0.075) + cashValue + otherAssetsValue;
     var zakatAmount = totalWealth * 0.025;
 
-    // Display Zakat amount
+    // Display Zakat amounts
     document.getElementById("zakatAmount").innerHTML = "Zakat Amount: $" + zakatAmount.toFixed(2);
 }
